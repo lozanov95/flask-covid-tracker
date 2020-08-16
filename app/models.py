3c069e06cbed2498e923b3a73b387a4593368568
lozanov95 <vasillozanov1@gmail.com>
2020-08-16 17:46:59 +0300

    added login functionality

--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
-from app import db
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
 
 class Cases(db.Model):
@@ -8,3 +10,23 @@ class Cases(db.Model):
     tests = db.Column(db.Integer)
     positive = db.Column(db.Float)
 
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- config.py
+++ config.py
@@ -7,3 +7,5 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '1w4VzIGGnm9$A9T@uPhzBsr8Xl'
+    
