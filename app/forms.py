fffadddce26588c4c2f0467c13a55bfd88a1146d
lozanov95 <vasillozanov1@gmail.com>
2020-08-18 07:10:37 +0300

    minor changes on the index page

--- app/forms.py
+++ app/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
 from app.models import User
 
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
