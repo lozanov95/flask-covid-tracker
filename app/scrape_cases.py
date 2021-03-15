49ec3fe224afe83e6308c602390837125d5495b0
lozanov95 <vasillozanov1@gmail.com>
2021-03-15 20:57:49 +0200

    reworked the CaseScrapper class

--- app/scrape_cases.py
+++ app/scrape_cases.py
@@ -1,31 +1,15 @@
 import requests
-import re
+import json
 
 
 class CaseScrapper:
     def __init__(self):
         self.cases, self.tests = self.scrape()
 
-    @staticmethod
-    def scrape_new_cases(text):
-        table = text.split('<table class="table">')
-        table = table[2]
-        numbers = re.findall(r'>([0-9]+)<', table)
-        new_cases = int(numbers[len(numbers) - 1])
-        return new_cases
-
-    @staticmethod
-    def scrape_new_tests(text):
-        table = text.split('<p class')
-        new_tests = int(re.search(r'[0-9\s]+', table[3]).group().replace(' ', ''))
-        return new_tests
-
     def scrape(self):
-        url = 'https://coronavirus.bg/bg/statistika'
-        r = requests.get(url).content.decode('utf-8')
-        new_cases = self.scrape_new_cases(r)
-        new_tests = self.scrape_new_tests(r)
-        return new_cases, new_tests
+        url = 'https://coronavirus.bg/stats.json'
+        covid_data = json.loads(requests.get(url).content)
+        return covid_data['infectedtoday'], covid_data['testedtoday']
 
     def get_stats(self):
         return self.cases, self.tests
